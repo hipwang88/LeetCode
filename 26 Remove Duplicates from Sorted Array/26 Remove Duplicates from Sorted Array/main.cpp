d05791c4b27d07b26b391e28a01cc5fe52a50373
haohao <hipwang88@gmail.com>
2015-03-24 22:44:51 +0800

    Solution 26: Remove Duplicates from Sorted Array 方法：两个指针步进比较，将唯一值往前移动，让数组的唯一数值都往前放置

---
+++ 26 Remove Duplicates from Sorted Array/26 Remove Duplicates from Sorted Array/main.cpp
@@ -0,0 +1,72 @@
+//
+//  main.cpp
+//  26 Remove Duplicates from Sorted Array
+//
+//  Created by Noamanwang on 15/3/24.
+//  Copyright (c) 2015年 Noamanwang. All rights reserved.
+//
+
+/*
+ Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+ 
+ Do not allocate extra space for another array, you must do this in place with constant memory.
+ 
+ For example,
+ Given input array A = [1,1,2],
+ 
+ Your function should return length = 2, and A is now [1,2].
+*/
+
+#include <iostream>
+
+class Solution
+{
+public:
+    int removeDumplicates(int A[], int n)
+    {
+        int nLength = 1;
+        
+        if (n > 0)
+        {
+            int *p = A;
+            int *q = A;
+            while (n)
+            {
+                if (*q == *p)
+                {
+                    ++q;
+                }
+                else
+                {
+                    ++p;
+                    *p = *q;
+                    ++q;
+                    ++nLength;
+                }
+                --n;
+            }
+        }
+        
+        return nLength;
+    }
+    
+    void outPutArray(int A[], int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            std::cout << A[i] << ' ' << std::endl;
+        }
+    }
+};
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    int sortedArray[] = {1,1,2,2,3,3,3,4,4,4,5,5,5,6,7,8,9,10,11,12,12,12,12,12,14,15,15};
+    
+    Solution s;
+    int newLength = s.removeDumplicates(sortedArray, sizeof(sortedArray)/sizeof(int));
+    s.outPutArray(sortedArray, newLength);
+
+    return 0;
+}
